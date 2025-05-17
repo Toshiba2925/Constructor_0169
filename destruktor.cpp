e2aa6d2bfe53f7bf2d2c609c8ea6ebe6ed1606fb
Toshiba2925 <aqifashiba25@gmail.com>
2025-05-17 17:08:59 +0700

    member funcion dengan constructor dan destructor

--- destruktor.cpp
+++ destruktor.cpp
@@ -14,3 +14,18 @@ public:
     void isiData();
 };
 
+// Definisi member Function
+angka::angka(int i)
+{ // Constructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+angka::angka()
+{ // Destructor
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
