22cb2b804ded3c7f74c860dba90216d64c36ae3e
Toshiba2925 <aqifashiba25@gmail.com>
2025-05-17 16:23:00 +0700

    membuat class dosen dengan akses private dan public

--- Constructor.cpp
+++ Constructor.cpp
@@ -14,3 +14,18 @@ public:
     friend void beriNilai(Dosen &d, Mahasiswa &m, float nilai);
 };
 
+class Dosen {
+private:
+    string nama;
+    string NIDN;
+    string pangkat;
+    float gaji;
+
+public:
+    Dosen(string n, string nidn, string p, float g) : nama(n), NIDN(nidn), pangkat(p), gaji(g) {}
+    void ubahPangkat(string pangkatBaru) {
+        pangkat = pangkatBaru;
+    }
+    friend void beriNilai(Dosen &d, Mahasiswa &m, float nilai);
+};
+
