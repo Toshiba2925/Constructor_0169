96eafe38d94ccafbeb0a40a2dffbbe18dbe8e36a
Toshiba2925 <aqifashiba25@gmail.com>
2025-05-17 16:19:50 +0700

    membuat kelas mahasiswa dengan akses private dan public

--- Constructor.cpp
+++ Constructor.cpp
@@ -3,3 +3,14 @@
 using namespace std;
 
 class Dosen;
+class Mahasiswa {
+private:
+    string nama;
+    string NIM;
+    float nilai;
+
+public:
+    Mahasiswa(string n, string nim) : nama(n), NIM(nim), nilai(0) {}
+    friend void beriNilai(Dosen &d, Mahasiswa &m, float nilai);
+};
+
